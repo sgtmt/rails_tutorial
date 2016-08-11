c671ddc30e6e833909413f938cab4fcd70c2062c
sgtmt <ainnuen@gmail.com>
2016-08-11 17:25:55 +0000

    (；・∀・)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "(；・∀・)ノ🍷(；・∀・)ノ🍻(；・∀・)ノ🍶"
+    render html: (；・∀・)ノ🍷(；・∀・)ノ🍻(；・∀・)ノ🍶・)ノ���"
 end
 end
